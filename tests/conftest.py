cc51459ab90ec8551d625b31689c634282731359
m.drogomeretskii <it@ekord.ru>
2025-06-01 17:36:08 +0300

    Задание №19: Вставить в тестовую базу тестовые данные

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import json
 import pytest
 
 from src.config import settings
-from src.database import Base, engine_null_pool, async_session_maker
+from src.database import Base, engine_null_pool, async_session_maker, async_session_maker_null_pull
 from src.schemas.hotels import Hotel
 from src.schemas.rooms import Room
 from src.utils.db_manager import DBManager
@@ -24,7 +24,7 @@ async def async_main():
     with open("tests/mock_rooms.json", "r", encoding="utf-8") as f:
         _rooms = [Room(**room) for room in json.load(f)]
 
-    async with DBManager(session=async_session_maker) as db:
+    async with DBManager(session=async_session_maker_null_pull) as db:
         await db.hotels.add_bulk(data=_hotels)
         await db.rooms.add_bulk(data=_rooms)
         await db.commit()
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,11 +1,13 @@
 import json
 
-from src.database import async_session_maker
+from src.database import async_session_maker_null_pull
 from src.schemas.hotels import Hotel
 from src.utils.db_manager import DBManager
 
 
 async def test_add_hotel():
     hotel_data = Hotel(title="OnlyOneStar", location="Anapa")
-    pass
+    async with DBManager(session=async_session_maker_null_pull) as db:
+        await db.hotels.add(data=hotel_data)
+        await db.commit()
 
